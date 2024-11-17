012657d71395a02b5e5d42449dd89dd1a4bf7ea7
Jisung Lee <jskurt9352@gmail.com>
2024-11-17 15:02:32 +0900

    feat(PR): update Search Engine Optimize

--- nextjs14-page-router/src/pages/search/index.tsx
+++ nextjs14-page-router/src/pages/search/index.tsx
@@ -5,6 +5,7 @@ import BookItem from '@/components/book-item'
 import fetchBooks from '@/lib/fetch-books'
 import { useRouter } from 'next/router'
 import { BookData } from '@/types'
+import Head from 'next/head'
 
 export default function Page() {
   const [books, setBooks] = useState<BookData[]>([])
@@ -23,6 +24,15 @@ export default function Page() {
   }, [q])
   return (
     <div>
+      <Head>
+        <title>Onebite books - search results</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="onebite books" />
+        <meta
+          property="og:description"
+          content="Lets explore the books registered in One Bite Books."
+        />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
