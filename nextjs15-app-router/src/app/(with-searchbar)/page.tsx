faf968b1bcca38de1d78f45b9c9e1c13dfcb4576
Jisung Lee <jskurt9352@gmail.com>
2024-11-18 11:38:17 +0900

    feat(AR): add UI

--- nextjs15-app-router/src/app/(with-searchbar)/page.tsx
+++ nextjs15-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,14 +1,22 @@
-import ClientComponent from '@/components/client-component'
-import styles from './page.module.css'
-import ServerComponent from '@/components/server-component'
+import BookItem from '@/components/book-item'
+import style from './page.module.css'
+import books from '@/mock/books.json'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      Index page
-      <ClientComponent>
-        <ServerComponent />
-      </ClientComponent>
+    <div className={style.container}>
+      <section>
+        <h3>Current recommended books</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>All registered books</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
     </div>
   )
 }
