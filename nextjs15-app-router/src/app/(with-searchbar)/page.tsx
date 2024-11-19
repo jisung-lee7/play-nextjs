fd892089a16ad2856bebe891872c87ad3f1538b6
Jisung Lee <jskurt9352@gmail.com>
2024-11-19 11:19:30 +0900

    feat(AR): add data fetching

--- nextjs15-app-router/src/app/(with-searchbar)/page.tsx
+++ nextjs15-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,52 @@
 import BookItem from '@/components/book-item'
 import style from './page.module.css'
-import books from '@/mock/books.json'
+import { BookData } from '@/types'
+
+async function AllBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`)
+  if (!response.ok) {
+    return <div>Error...</div>
+  }
+
+  const allBooks: BookData[] = await response.json()
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  )
+}
+
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  )
+  if (!response.ok) {
+    return <div>Error...</div>
+  }
+
+  const recoBooks: BookData[] = await response.json()
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>Current recommended books</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>All registered books</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   )
