7cbc194752fb4c02a1916e6451251969a64fdcf9
Jisung Lee <jskurt9352@gmail.com>
2024-11-19 13:12:26 +0900

    feat: set cache next revalidate

--- nextjs15-app-router/src/app/(with-searchbar)/page.tsx
+++ nextjs15-app-router/src/app/(with-searchbar)/page.tsx
@@ -25,7 +25,7 @@ async function AllBooks() {
 async function RecoBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
-    { cache: 'force-cache' }
+    { next: { revalidate: 3 } }
   )
   if (!response.ok) {
     return <div>Error...</div>
