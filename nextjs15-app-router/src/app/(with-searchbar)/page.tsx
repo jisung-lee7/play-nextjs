77933e650e0fc7b944c95a550b0057be624d4d1c
Jisung Lee <jskurt9352@gmail.com>
2024-12-10 10:48:32 +0900

    feat(AR): add client and server component

--- nextjs15-app-router/src/app/(with-searchbar)/page.tsx
+++ nextjs15-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,14 @@
+import ClientComponent from './client-component'
 import styles from './page.module.css'
+import ServerComponent from './server-component'
 
 export default function Home() {
-  return <div className={styles.page}>Index page</div>
+  return (
+    <div className={styles.page}>
+      Index page
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  )
 }
