b925f695a02ddd2b9b6666e52bc3c4f9b0488569
Jisung Lee <jskurt9352@gmail.com>
2025-01-07 15:24:56 +0900

    feat(AR): add generateStaticParams

--- nextjs15-app-router/src/app/book/[id]/page.tsx
+++ nextjs15-app-router/src/app/book/[id]/page.tsx
@@ -1,5 +1,9 @@
 import style from './page.module.css'
 
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
+
 export default async function Page({
   params
 }: {
