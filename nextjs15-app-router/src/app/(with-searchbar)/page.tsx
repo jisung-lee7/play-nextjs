15224850843719ac77c36430be65c9058e753d0f
Jisung Lee <jskurt9352@gmail.com>
2025-01-08 12:45:10 +0900

    feat(AR): add streaming with suspense component

--- nextjs15-app-router/src/app/(with-searchbar)/page.tsx
+++ nextjs15-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,11 @@
 import BookItem from '@/components/book-item'
 import style from './page.module.css'
 import { BookData } from '@/types'
+import { delay } from '@/util/delay'
+import { Suspense } from 'react'
 
 async function AllBooks() {
+  await delay(1500)
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: 'force-cache' }
@@ -23,6 +26,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000)
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -41,16 +45,22 @@ async function RecoBooks() {
   )
 }
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>Current recommended books</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>Loading books...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>All registered books</h3>
-        <AllBooks />
+        <Suspense fallback={<div>Loading books...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   )
